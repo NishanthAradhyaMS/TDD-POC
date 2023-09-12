2c7b9bbeaab43d830c1f5e57a851dd9e734afe0d
nishantharadhyams <nishantharadhya.marballisomashekar@cesltd.com>
2023-09-12 15:43:46 +0530

    Added Integration test for UserController

---
+++ TodoApp.IntegrationTesting/System/Controller/UserControllerTest.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using TodoApp.Controllers;
+using Microsoft.Extensions.Options;
+using TodoApp.Models;
+using FluentAssertions;
+using TodoApp.IntegrationTesting.Fixtures;
+
+namespace TodoApp.IntegrationTesting.System.Controller
+{
+    public class UserControllerTest
+    {
+        [Fact]
+        public async Task Get_OnSuccess_ReturnStatusCode200()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result= (OkResult)await sut.GetAync();
+
+            // Assert
+            result.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task Post_OnSuccess_ReturnStatusCode201()
+        {
+            // Arrange
+            var sut = new UserController();
+            
+            // Act
+            var result = (CreatedResult)await sut.PostAsyn(UserFixture.GetUser());
+            // Assert
+            result.StatusCode.Should().Be(201);
+            
+        }
+        [Fact]
+        public async Task Post_OnBadrequest_Return400()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result = (BadRequestResult)await sut.PostAsyn(UserFixture.UserWithBadRequest());
+            // Assert
+            result.StatusCode.Should().Be(400);
+            Assert.IsType<BadRequestResult>(result);
+
+        }
+
+        [Fact]
+        public async Task GetUserById_OnBadRequest_ReturnStatusCode400()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result = (BadRequestResult)await sut.GetUserByIdAsync(UserFixture.UserInvalidId());
+            // Assert
+            result.StatusCode.Should().Be(400);
+            Assert.IsType<BadRequestResult>(result);
+
+        }
+        [Fact]
+        public async Task GetUserById_OnSuccess_ReturnStatusCode200()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result = (OkResult)await sut.GetUserByIdAsync(UserFixture.UservalidId());
+
+            // Assert
+            result.StatusCode.Should().Be(200);
+
+        }
+
+        [Fact]
+        public async Task UpdateUser_OnInvalidId_ReturnBadRequest_WithStatusCode400()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result = (BadRequestResult)await sut.UpdateUserAsync(UserFixture.UserInvalidId(),UserFixture.GetUser());
+            // Assert
+            result.StatusCode.Should().Be(400);
+            Assert.IsType<BadRequestResult>(result);
+
+        }
+
+        [Fact]
+        public async Task UpdateUser_OnInvalidUser_ReturnBadRequest_WithStatusCode400()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result = (BadRequestResult)await sut.UpdateUserAsync(UserFixture.UservalidId(), UserFixture.UserWithBadRequest());
+            // Assert
+            result.StatusCode.Should().Be(400);
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteUser_OnInvalidUserId_ReturnBadRequest_WithStatusCode400()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result = (BadRequestResult)await sut.DeleteUserAsync(UserFixture.UserInvalidId());
+            // Assert
+            result.StatusCode.Should().Be(400);
+            Assert.IsType<BadRequestResult>(result);
+
+        }
+
+        [Fact]
+        public async Task DeleteUser_OnSuccess_ReturnOk_WithStatusCode200()
+        {
+            // Arrange
+            var sut = new UserController();
+
+            // Act
+            var result = (OkResult)await sut.DeleteUserAsync(UserFixture.UservalidId());
+            // Assert
+            result.StatusCode.Should().Be(200);
+            Assert.IsType<OkResult>(result);
+
+        }
+
+
+
+    }
+}
---
+++ TodoApp/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAync()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsyn(User user)
+        {
+            if (user == null || String.IsNullOrEmpty(user.FirstName))
+                return BadRequest();
+            return Created("",user);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetUserByIdAsync(string id)
+        {
+            if (!ObjectId.TryParse(id, out _))
+            {
+                return BadRequest();
+            }
+            
+            return Ok();
+        }
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateUserAsync(string id,User user)
+        {
+            if (!ObjectId.TryParse(id, out _) || String.IsNullOrEmpty(user.FirstName))
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+
+
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteUserAsync(string id)
+        {
+            if (!ObjectId.TryParse(id, out _))
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+
+
+        }
+    }
+}
